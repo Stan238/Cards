39280d57e67eb75a2fac20fe7a022cdba2a8f03a
Stan238 <127895826+Stan238@users.noreply.github.com>
2024-06-16 21:03:00 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Stan U on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cards/ContentView.swift
@@ -0,0 +1,42 @@
+    //
+    //  ContentView.swift
+    //  Cards
+    //
+    //  Created by Stan U on 6/16/24.
+    //
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Image(systemName: "lanyardcard")
+//                .resizable()
+//                .scaledToFit()
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//                .symbolRenderingMode(.multicolor)
+//                .colorMultiply(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+//                .background(.teal)
+//            Spacer()
+            VStack{
+
+            }
+            .frame(width: 250, height: 450, alignment: .center)
+            .padding()
+            .background(Color(hue: 1.0, saturation: 0.027, brightness: 0.958))
+            .clipShape(RoundedRectangle(cornerRadius: 40), style: FillStyle()
+                
+                )
+            .shadow(color: .gray,    radius: 30)
+            .padding()
+//            .border(Color.black)
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
